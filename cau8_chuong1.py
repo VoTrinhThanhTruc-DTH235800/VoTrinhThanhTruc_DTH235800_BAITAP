419ece4d5e144867f50c053b5a5d82b9e68433c3
VoTrinhThanhTruc-DTH235800 <truc_dth235800@student.agu.edu.vn>
2025-10-13 21:53:18 +0700

    Add files via upload

---
+++ cau8_chuong1.py
@@ -0,0 +1,15 @@
+
+# sử dụng vòng lặp 
+n = 4  # số hàng
+
+for i in range(n):
+    if i == 0 or i == n - 1:  # hàng đầu và hàng cuối
+        print("****")
+    else:  # các hàng giữa
+        print("*  *")
+
+# sử dụng câu lệnh in
+print("****")
+print("*  *")
+print("*  *")
+print("****")
