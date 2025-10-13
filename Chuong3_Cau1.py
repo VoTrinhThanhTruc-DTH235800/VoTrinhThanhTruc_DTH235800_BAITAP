1256d8c9123e23d39c0d399d29877f4780f252ed
VoTrinhThanhTruc-DTH235800 <truc_dth235800@student.agu.edu.vn>
2025-10-13 21:59:27 +0700

    Delete Chuong3_Cau1.py

--- Chuong3_Cau1.py
+++
@@ -1,6 +0,0 @@
-print("Chuong trinh kiem tra nam nhuan");
-year = int (input("Moi ban nhap vao 1 nam bat ky: "))
-if(year % 4 ==0 and year % 100 !=0) or year % 400 ==0:
-    print("Nam", year, "la nam nhuan");
-else:
-    print("Nam", year, "khong nhuan");
