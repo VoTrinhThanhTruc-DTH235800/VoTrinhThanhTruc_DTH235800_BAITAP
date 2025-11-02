0b812328d5dab7f3e4bcbfba7be3ddf688db396b
VoTrinhThanhTruc-DTH235800 <truc_dth235800@student.agu.edu.vn>
2025-11-02 22:56:52 +0700

    Add files via upload

---
+++ Chuong5_Cau4.py
@@ -0,0 +1,32 @@
+﻿
+"""
+Trình bày một số hàm quan trọng trong xử lý Chuỗi của Python
+
+len(s): Trả về độ dài chuỗi
+  → `len("Hello") = 5`
+
+str(x): Chuyển giá trị sang chuỗi
+  → `str(123) = "123"`
+
+s.lower(): Chuyển chuỗi thành chữ thường
+  → `"HeLLo".lower() = "hello"`
+
+s.upper(): Chuyển chuỗi thành chữ hoa
+  → `"HeLLo".upper() = "HELLO"`
+
+s.capitalize(): Viết hoa ký tự đầu tiên của chuỗi
+  → `"python".capitalize() = "Python"`
+
+s.title(): Viết hoa ký tự đầu của mỗi từ
+  → `"xin chao ban".title() = "Xin Chao Ban"`
+
+s.strip(): Xóa khoảng trắng ở đầu và cuối chuỗi
+  → `"  hi  ".strip() = "hi"`
+
+s.lstrip(): Xóa khoảng trắng bên trái chuỗi
+  → `"   hi".lstrip() = "hi"`
+
+s.rstrip(): Xóa khoảng trắng bên phải chuỗi
+  → `"hi   ".rstrip() = "hi"`
+
+"""
